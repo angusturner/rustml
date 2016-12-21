03239a3be18eb3e2b0a8380ede818ca1c72281cf
Angus Turner <angusturner27@gmail.com>
2016-12-21 17:16:36 +1000

    did some stuff

--- nn/src/nn.rs
+++ nn/src/nn.rs
@@ -21,6 +21,7 @@ impl NN {
         }
     }
 
+    /// return the weights
     pub fn get_weights(&self) -> Vec<Matrix<f64>> {
         self.weights.clone()
     }
@@ -73,18 +74,67 @@ impl NN {
         }
     }
 
-    /// train the neural net using gradient descent
+    /// train the neural net using batch gradient descent
     /// X - the design matrix of training examples
     /// y - the response matrix (output values mapped to one-hot vectors)
     /// lambda - regularization parameter
     /// alpha - learning rate
     /// max_iters - number of iterations of gradient descent to perform
-    fn train(&self, X: Matrix<f64>, y: Matrix<f64>, lambda: f64, alpha: f64, max_iters: usize) -> bool {
-        true
+    pub fn train(&self, X: Matrix<f64>, y: Matrix<f64>, lambda: f64, alpha: f64, max_iters: usize) -> bool {
+
     }
 
+    fn cost_fn(X: &Matrix<f64>, y: &Matrix<f64>, theta1: &Matrix<f64>, theta2: &Matrix<f64>, lambda: &f64)
+        -> (f64, Matrix<f64>, Matrix<f64>) {
+
+        // compute activations on the hidden layer
+        let z2 = X*theta1.transpose();
+        let mut a2 = z2.clone().apply(&sigmoid);
+        a2 = add_ones(&a2); // add bias units
+
+        // compute activations on the output layer
+        let z3 = &a2*theta2.transpose();
+        let h = z3.apply(&sigmoid);
+
+        // get dimensions of y
+        let (m, r) = dims(&y);
+
+        // compute the cost: -1/m * [ sum ( y.*log(h) + (1-y).*log(1-h) ) ]
+        let ones: Matrix<f64> = Matrix::new(m, r, vec![1f64; m*r]); // needed because f64 - Matrix is not valid :(
+        let cost = y.elemul(&log(&h)) + (&ones-y).elemul(&log(&(&ones-&h)));
+        let mut J: f64 = - cost.sum() / (m as f64); // switch signs, take the mean
+
+        // zero the bias unit columns in the parameter matrices
+        let theta1_0 = zero_first_col(&theta1);
+        let theta2_0 = zero_first_col(&theta2);
+
+        // add regularization: lambda/2m * [ sum(theta1.^2) + sum(theta2.^2) ]
+        J += (lambda/(2f64 * (m as f64))) * (theta1_0.elemul(&theta1_0).sum() + theta2_0.elemul(&theta2_0).sum());
+
+        // compute the errors on the output
+        let d3 = h - y;
+
+        // get a slice into theta2, with the first column (pertaining to bias units) removed
+        let theta2_1 = MatrixSlice::from_matrix(&theta2, [0, 1], theta2.rows(), theta2.cols()-1);
+
+        // compute errors on the hidden layer (does not include bias unit)
+        let d2 = (theta2_1.transpose()*d3.transpose()).transpose().elemul(&(z2.apply(&sigmoid_gradient)));
+
+        // compute gradients
+        let mut theta1_grad = d2.transpose()*(X / (m as f64));
+        let mut theta2_grad = d3.transpose()*(a2 / (m as f64));
+
+        // regularize gradients
+        theta1_grad += theta1_0 * (lambda/(m as f64));
+        theta2_grad += theta2_0 * (lambda/(m as f64));
+
+        (J, theta1_grad, theta2_grad)
+    }
+
+
+
     /// use the trained weights to generate predictions
-    fn predict(&self, test_set: Matrix<f64>) {
+    pub fn predict(&self, test_set: Matrix<f64>) {
 
     }
 }
